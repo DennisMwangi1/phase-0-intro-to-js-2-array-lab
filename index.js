315f54b46d1708a8076fcf7e7a9e90d210363586
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-02 11:03:27 +0300

    commit tree ceaa6a801dccd1c2a8dda6c3fb32c53fb2d20d36

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Milo")
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Bob")
+}
+function appendCat(name){
+
+    const copycats = [...cats,"Broom"]
+    return copycats;
+   
+}
+function prependCat(name){
+   const copycats =["Arnold", ...cats]
+   return copycats;
+}
+function removeLastCat(){
+  const copycats =  cats.slice(0,2);
+  return copycats;
+}
+function removeFirstCat(){
+   const copycats = cats.slice(1);
+  return copycats;
+}
